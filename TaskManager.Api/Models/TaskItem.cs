4c45e768eae04b2276eec9488db43e12b943c6cf
Dvurukaya <julgorina@yandex.ru>
2025-08-13 17:06:39 +0300

    Загрузка начальных этапов проекта для возможности продолжать процесс с других устройств

---
+++ TaskManager.Api/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManager.Api.Models
+
+{
+    public enum TaskStatus { ToDo = 0, InProgress = 1, Done = 2 }
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.ToDo;
+        public int? Priority { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
